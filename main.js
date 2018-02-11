1a5a8580c5e7d58ed26682e7fbfe2490d6f2dd8d
Andrew Salcedo <asalcedo07@gmail.com>
2018-02-11 18:00:57 +0100

    Add: Styling and slight changes to html.

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
-
-const message = '<div id="message"><button id="quit">Close the BBC tab</button><div id="continue">I\'m going to read something interesting.</div></div>';
+const message = `
+	<div id="message">
+  		<h1 class="h1">You don't want to do this. The news can wait.</h1>
+  		<h2 class="h2"> Let's stay focused. Come on, close the tab and come back later.</h2>
+  		<button id="close-tab">Close the Tab and Read the News Later</button>
+  		<div id="continue">Time for a break. Going to read something interesting.</div>   
+ 	</div>`;
 
 preventDistraction();
 
 function preventDistraction() {
-	const page = document.querySelector("body");
-	page.insertAdjacentHTML(
-        "afterbegin",
-        message 
-	);
+  const page = document.querySelector('body');
+  page.insertAdjacentHTML('afterbegin', message);
 }
 
-const closeTab = document.getElementById("quit");
-closeTab.addEventListener("click", () => chrome.runtime.sendMessage({ closeThis: true }) );
+const closeTab = document.getElementById('close-tab');
+closeTab.addEventListener('click', () =>
+  chrome.runtime.sendMessage({ closeThis: true })
+);
 
-const readNews = document.getElementById("continue");
-readNews.addEventListener("click", () => {
-	const msg = document.getElementById("message");
-	document.querySelector("body").removeChild(msg);
-	document.getElementById("appMountPoint").style.display = "block";
+const readNews = document.getElementById('continue');
+readNews.addEventListener('click', () => {
+  const msg = document.getElementById('message');
+  document.querySelector('body').removeChild(msg);
+  document.getElementById('appMountPoint').style.display = 'block';
 });
